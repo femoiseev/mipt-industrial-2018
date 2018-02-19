042755b2003d1016520060ea753e222d972b2714
Fedor Moiseev <femoiseev@gmail.com>
2018-02-20 02:23:00 +0300

    Fix indentation

--- 1/producer/producer.py
+++ 1/producer/producer.py
@@ -7,11 +7,11 @@ channel.queue_declare(queue='hello')
 message = None
 
 while True:
-	try:
-		message = input('> ')
-	except EOFError:
-		break
+    try:
+        message = input('> ')
+    except EOFError:
+        break
 
-	channel.basic_publish(exchange='', routing_key='hello', body=message)
+    channel.basic_publish(exchange='', routing_key='hello', body=message)
 
 connection.close()
